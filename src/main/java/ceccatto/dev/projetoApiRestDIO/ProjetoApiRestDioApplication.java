0fb6a8bfde1c30bb81616de8cda19d6af6a47761
Jh.Ceccatto <jbom6@hotmail.com>
2023-10-22 17:16:51 -0300

    Initial commit

---
+++ src/main/java/ceccatto/dev/projetoApiRestDIO/ProjetoApiRestDioApplication.java
@@ -0,0 +1,13 @@
+package ceccatto.dev.projetoApiRestDIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiRestDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ceccatto/dev/projetoApiRestDIO/ProjetoApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package ceccatto.dev.projetoApiRestDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
